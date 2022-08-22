506371df909764ca3f3d5bcfe4c5f22bec4876c0
Raminka <raminka13@hotmail.com>
2022-08-22 11:39:25 -0500

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Missions from './components/Missions';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={<Rockets />} /> */}
+        <Route path="/missions" element={<Missions />} />
+        {/* <Route path="/profile" element={<Profile />} /> */}
+      </Routes>
+    </>
   );
 }
 
