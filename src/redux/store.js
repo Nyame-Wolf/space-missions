fa6a3914fa3549825fd8a4552e6f6192e17e8689
Nyame-Wolf <mumenyam@gmail.com>
2022-08-23 13:42:29 +0300

    Add redux logger to the project

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import logger from 'redux-logger';
+import rocketReducer from './rockets/rockets';
 
 const store = configureStore({
-//   reducer: {
-//     rockets: ,
-//     missions: ,
-//   },
+  reducer: {
+    rockets: rocketReducer,
+    //     missions: ,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
