5a69d7dd1db94fb8bd94a9a9e4e3b61a281900fd
Nyame-Wolf <mumenyam@gmail.com>
2022-08-23 13:35:47 +0300

    Display rockets on the user interface

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,3 +1,28 @@
-const Rockets = () => <div>My Rocket</div>;
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Rocket from '../components/Rocket';
+import { getRockets } from '../redux/rockets/rockets';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  return (
+
+    <div className="rocket-list">
+      <div className="rockets-container">
+        {rockets.map((rocket) => (
+          <ul key={rocket.id}>
+            <Rocket rocket={rocket} />
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Rockets;
