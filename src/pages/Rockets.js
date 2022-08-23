1ea5a1a0e2c61a28add2feeae110f720484486dc
Nyame-Wolf <mumenyam@gmail.com>
2022-08-23 13:38:10 +0300

    Fi;ter rockets to show on my profile page

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -3,7 +3,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import Rocket from '../components/Rocket';
 import { getRockets } from '../redux/rockets/rockets';
 
-const Rockets = () => {
+export const Rockets = (props) => (
+
+  <div className="rocket-list">
+    <div className="rockets-container">
+      {props.rockets.map((rocket) => (
+        <ul key={rocket.id}>
+          <Rocket rocket={rocket} />
+        </ul>
+      ))}
+    </div>
+  </div>
+);
+
+export function MyRockets() {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets);
 
@@ -12,17 +25,19 @@ const Rockets = () => {
   }, []);
 
   return (
-
-    <div className="rocket-list">
-      <div className="rockets-container">
-        {rockets.map((rocket) => (
-          <ul key={rocket.id}>
-            <Rocket rocket={rocket} />
-          </ul>
-        ))}
-      </div>
-    </div>
+    <Rockets rockets={rockets.filter((rocket) => rocket.reserved)} />
   );
-};
+}
 
-export default Rockets;
+export default function AllRockets() {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  return (
+    <Rockets rockets={rockets} />
+  );
+}
