59ecc44ec78af10f1650393a76282429ca72e3b2
Raminka <raminka13@hotmail.com>
2022-08-24 14:41:39 -0500

    Added Dragons Page, Slice and Components

---
+++ src/components/DragonItem.js
@@ -0,0 +1,41 @@
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { reserveDragon } from '../redux/dragons/dragonsSlice';
+
+const DragonItem = (props) => {
+  const dispatch = useDispatch();
+  const { dragon } = props;
+  return (
+    <div className="rocket-container">
+      <img src={dragon.flickr_images} alt="dragon" className="rocket-img" />
+      <div className="text-style">
+        <h3>{dragon.name }</h3>
+        <div>
+          {dragon.reserved
+            ? (
+              <p>
+                <span className="reserved-success">reserved</span>
+                {' '}
+                {dragon.description}
+              </p>
+            )
+            : <p>{dragon.description}</p>}
+        </div>
+        <button className={dragon.reserved ? 'cancel-rocket' : 'reserve'} type="button" onClick={() => dispatch(reserveDragon({ dragon }))}>{dragon.reserved ? 'Cancel Reservation' : 'Reserve'}</button>
+      </div>
+    </div>
+  );
+};
+
+DragonItem.propTypes = {
+  dragon: PropTypes.shape(
+    {
+      id: PropTypes.string,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      reserved: PropTypes.bool,
+      flickr_images: PropTypes.arrayOf(PropTypes.string),
+    },
+  ).isRequired,
+};
+export default DragonItem;
