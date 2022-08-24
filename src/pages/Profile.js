02524c9a3181d4dcdf7f57299bff8ccd71bed434
Raminka <raminka13@hotmail.com>
2022-08-24 12:12:46 -0500

    Add styling for Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -13,36 +13,45 @@ const Profile = () => {
   return (
     <div className="profile-page">
       <ul className="missions-profile">
+        <h2>Rockets Reserved</h2>
+        {rocketsReserved.length === 0
+          ? <li className="no-mission">No Rockets Reserved</li>
+          : rocketsReserved.map((rocket) => (
+            <li key={rocket.id} className="mission-wrapper">
+              <h4 className="name">
+                {rocket.rocket_name}
+                <a href={rocket.wikipedia} target="blanc">
+                  ➕
+                </a>
+              </h4>
+              <button
+                className={rocket.reserved ? 'leave-mission' : 'join'}
+                type="button"
+                onClick={() => dispatch(reserve({ rocket }))}
+              >
+                {rocket.reserved ? 'Cancel Rocket' : 'Join Rocket' }
+              </button>
+            </li>
+          ))}
+      </ul>
+      <ul className="missions-profile">
         <h2>Missions Reserved</h2>
         {missionReserved.length === 0
-          ? <li className="mission-wrapper">No Missions Reserved</li>
+          ? <li className="no-mission">No Missions Reserved</li>
           : missionReserved.map((mission) => (
             <li key={mission.mission_id} className="mission-wrapper">
-              <h4 className="name">{mission.mission_name}</h4>
+              <h4 className="name">
+                {mission.mission_name}
+                <a href={mission.wikipedia} target="blanc">
+                  ➕
+                </a>
+              </h4>
               <button
                 className={mission.reserved ? 'leave-mission' : 'join'}
                 type="button"
                 onClick={() => dispatch(reserveMission({ mission }))}
               >
                 {mission.reserved ? 'Cancel Mission' : 'Join Mission' }
-              </button>
-            </li>
-          ))}
-      </ul>
-      <div className="rockets-profile" />
-      <ul className="missions-profile">
-        <h2>Rockets Reserved</h2>
-        {rocketsReserved.length === 0
-          ? <li className="mission-wrapper">No rockets Reserved</li>
-          : rocketsReserved.map((rocket) => (
-            <li key={rocket.id} className="mission-wrapper">
-              <h4 className="name">{rocket.rocket_name}</h4>
-              <button
-                className={rocket.reserved ? 'leave-mission' : 'join'}
-                type="button"
-                onClick={() => dispatch(reserve({ rocket }))}
-              >
-                {rocket.reserved ? 'Cancel Mission' : 'Join Mission' }
               </button>
             </li>
           ))}
