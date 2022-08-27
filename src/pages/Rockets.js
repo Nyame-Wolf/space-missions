975e54d57d1abe1ccda16f5671f6708b3262f0e4
Nyame-Wolf <mumenyam@gmail.com>
2022-08-27 11:41:48 +0300

    Add is loading usecase

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import FullRocket from '../components/rocket/Rocket';
@@ -8,26 +8,32 @@ import '../components/rocket/Rockets.css';
 export default function Rockets({ rocketFilter, component: Rocket }) {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (!rockets.length) {
-      dispatch(getRockets());
+    if (!rockets.length && !isLoading) {
+      setLoading(true);
+      dispatch(getRockets()).then(() => {
+        setLoading(false);
+      }).catch(() => {
+        setLoading(false);
+      });
     }
   }, []);
 
-  return (
-
+  console.log(rockets, isLoading);
+  const filteredRockets = rockets.filter(rocketFilter);
+  return filteredRockets.length ? (
     <div className="rockets-container">
       <ul className="rockets-list">
-        {/* {rockets.map((rocket) => ( */}
-        {rockets.filter(rocketFilter).map((rocket) => (
+        { filteredRockets.map((rocket) => (
           <li key={rocket.id}>
             <Rocket rocket={rocket} />
           </li>
         ))}
       </ul>
     </div>
-  );
+  ) : <div className="no-mission">No Rockets Reserved</div>;
 }
 
 Rockets.propTypes = {
